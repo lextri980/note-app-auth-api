5b8e2343998ff36802fe2b8a8d1dd083e07fc315
lextri980 <lextri980@gmail.com>
2023-11-23 19:09:57 +0700

    update

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -9,7 +9,7 @@ const User = require('../models/User')
 // @access Public
 router.get('/', verifyToken, async (req, res) => {
     try {
-        const user = await User.findById(req.userId)
+        const user = await User.findById(req.userId).select("-password")
         if (!user)
             return res.status(400).json({
                 success: false,
